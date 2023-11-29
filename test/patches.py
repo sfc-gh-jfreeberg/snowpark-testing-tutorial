8608251b4835b244555ea118c6a442e55f27447d
Jason Freeberg <jason.freeberg@snowflake.com>
2023-11-29 10:38:48 -0800

    Update patches.py

--- test/patches.py
+++ test/patches.py
@@ -1,7 +1,7 @@
 from snowflake.snowpark.mock.functions import patch
 from snowflake.snowpark.functions import monthname
-from snowflake.snowpark.mock.snowflake_data_type import ColumnEmulator
-from snowflake.snowpark.types import TimestampType
+from snowflake.snowpark.mock.snowflake_data_type import ColumnEmulator, ColumnType
+from snowflake.snowpark.types import StringType
 import datetime
 import calendar
 
@@ -10,5 +10,5 @@ def patch_monthname(column: ColumnEmulator) -> ColumnEmulator:
     ret_column = ColumnEmulator(data=[
         calendar.month_abbr[datetime.datetime.strptime(row, '%Y-%m-%d %H:%M:%S.%f %z').month]
         for row in column])
-    ret_column.sf_type = TimestampType()
+    ret_column.sf_type = ColumnType(StringType(), True)
     return ret_column
